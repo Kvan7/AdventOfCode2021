022c20df03f9f2a26f9e89dff123ab7d5f8e090d
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2021-12-07 21:50:06 -0600

    Create day7.cpp

---
+++ day7.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+vector<int> inCSV()
+{
+	string str;
+	cin >> str;
+	vector<int> vect;
+	std::stringstream ss(str);
+	for (int i; ss >> i;) {
+        vect.push_back(i);
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+    return vect;
+}
+
+
+const int DAYS = 256;
+
+
+
+
+int main()
+{
+	vector<int> crabs;
+	int median, fuel1 = 0, fuel2 = 0;
+	crabs = inCSV();
+	sort(crabs.begin(),crabs.end());
+	median = crabs[floor(crabs.size()/2)];
+	cout<< "m:"<<median<<endl;
+
+	int mean =accumulate(crabs.begin(),crabs.end(), 0)/crabs.size();
+	cout<<"AVG:"<<mean<<endl;
+
+	FOREACH(it, crabs)
+	{
+		fuel1+=abs(median-*it);
+		fuel2+=abs(mean-*it)*(1+abs(mean-*it))/2;
+	}
+
+	cout<<"FUEL1:"<<fuel1<<endl;
+	cout<<"FUEL2:"<<fuel2<<endl;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
